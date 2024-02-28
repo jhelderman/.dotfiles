0ac6ea54435e8455bbb766d74918b9008352bf73
jordan <jheld2010@gmail.com>
2024-02-28 16:01:30 -0500

    remove indent-blankline

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,16 +154,14 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+--  {
+    -- -- Add indentation guides even on blank lines
+    -- 'lukas-reineke/indent-blankline.nvim',
+    -- -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- -- See `:help indent_blankline.txt`
+    -- main = "ibl",
+    -- opts = {},
+  -- },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
