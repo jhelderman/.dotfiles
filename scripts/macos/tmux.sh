5b6227be45c314aec9f21f445ce776b98baded2a
jordan <jheld2010@gmail.com>
2025-03-29 22:30:47 -0400

    tweak tmux configuration

--- scripts/macos/tmux.sh
+++ scripts/macos/tmux.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 repo_root=$(dirname $(dirname $(cd $(dirname "${BASH_SOURCE[0]}") && pwd)))
 brew install tmux
+# install tmux plugin manager
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
 # install configuration
-rm $HOME/.tmux 2> /dev/null | true
-ln -s $repo_root/.tmux/ $HOME/.tmux
-rm $HOME/.tmux.conf 2> /dev/null | true
-ln -s $repo_root/.tmux.conf $HOME/.tmux.conf
+rm $HOME/.config/tmux 2> /dev/null | true
+ln -s $repo_root/tmux/ $HOME/.config/tmux
