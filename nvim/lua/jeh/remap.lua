73a8a53f63a0ec7939fb153824d2aa654209ce2a
jordan <jheld2010@gmail.com>
2025-03-29 23:44:06 -0400

    shortcuts for sessionizer

--- nvim/lua/jeh/remap.lua
+++ nvim/lua/jeh/remap.lua
@@ -19,6 +19,7 @@ vim.keymap.set("n", "<leader>Y", [["*Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "tmux sessionizer" })
 
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
