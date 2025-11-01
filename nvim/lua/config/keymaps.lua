26245e65106aba4c785a0c5868b944cac97a4f5f
jordan <10335331+jhelderman@users.noreply.github.com>
2025-11-01 10:04:29 -0400

    save keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- basic
+vim.keymap.set("n", "<leader><cr>", "<cmd>w<cr>", { desc = "Write" })
+
 -- oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
