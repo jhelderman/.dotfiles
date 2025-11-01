6b9239b78f3ac6a5fe5a7a24c4a5d41866aeb20b
jordan <10335331+jhelderman@users.noreply.github.com>
2025-11-01 08:24:44 -0400

    update neovim to 0.11.4

--- scripts/macos/neovim.sh
+++ scripts/macos/neovim.sh
@@ -4,16 +4,16 @@ repo_root=$(dirname $(dirname $(cd $(dirname "${BASH_SOURCE[0]}") && pwd)))
 brew install ninja cmake gettext curl
 # get the code
 if [ ! -d $HOME/code/neovim ]; then
-    git clone https://github.com/neovim/neovim.git $HOME/code/neovim
+	git clone https://github.com/neovim/neovim.git $HOME/code/neovim
 fi
 cd $HOME/code/neovim
 git fetch
-git checkout v0.11.0
+git checkout v0.11.4
 # build
 make CMAKE_BUILD_TYPE=RelWithDebInfo --prefix=$HOME/.local
 # install binaries
-make CMAKE_INSTALL_PREFIX=$HOME/.local install
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/.local install
 # install configuration
-mkdir -p $HOME/.config 2> /dev/null | true
-rm -rf $HOME/.config/nvim 2> /dev/null | true
+mkdir -p $HOME/.config 2>/dev/null | true
+rm -rf $HOME/.config/nvim 2>/dev/null | true
 ln -s $repo_root/nvim $HOME/.config/nvim
